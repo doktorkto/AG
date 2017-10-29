d5dfb9ff47cf8091e1a207a67536154f08e3ca4a
doktorkto <32988757+doktorkto@users.noreply.github.com>
2017-10-29 10:52:54 +0300

    27-10-1

--- 27.0-1.c
+++ 27.0-1.c
@@ -3,32 +3,48 @@
 
 #include <stdio.h>
 
+float min(float a, float b, float c){
+for (int i=0;i<=2;i++){
+    if (a>b)
+            if (b<c) return b;
+    else return c;
+    else return a;}}
+float max(float a, float b, float c){
+for (int i=0;i<=2;i++){
+    if (a<b)
+            if (b>c) return b;
+    else return c;
+    else return a;}
+}
+float med(float a, float b, float c){
+    int n[2];
+    n[0]=a;
+    n[1]=b;
+    n[3]=c;
+    for (int i=1;i<=2;i++) {for (int j;j<=i-1;j++){
+        if (n[i]<n[j]) n[i]=n[j];n[j]=n[i];}}
+        a=n[0];
+        b=n[1];
+        c=n[2];
+        return b;
+    }
+void swap(float a, float b, float c){
+    float t;
+    t=c;
+    c=b;
+    b=a;
+    a=t;
+}
+float avg(float a, float b, float c){
+    float d;
+    return (a+b+c)/3.0;
+
+}
 int main () {
-float a[4];
-for (int i = 1;i < 4;a[i++] = 0.0f);
-for (int i = 1;i <= 3;i++) scanf("%f", &a[i]);
-float b;
-b=a[1];
-for (int i=1;i<=3;i++){
-    if (b>a[i]) b=a[i];
-}
-printf("min = %g", b);
-float c;
-c=0;
-for (int i=1;i<=3;i++){
-    if (c<a[i]) c=a[i];}
-printf("\nmax = %g", c);
-float g;
-g=(a[1]+a[2]+a[3])/3;
-printf("\navg = %g", g);
-float o;
-o=0;
-for (int i=1;i<=3;i++){
-    if (a[i]>b && a[i]<c){
-        o=a[i];}
-}
-printf("\nmed = %g", o);
-float t;
-printf("\n1 = %g 2 = %g 3 = %g", a[1], a[2], a[3]);
-printf("\n1 = %g 2 = %g 3 = %g", a[3], a[1], a[2]);
+float d[3];
+scanf("%g %g %g", &d[0], &d[1], &d[2]);
+printf("min = %g", min(d[0],d[1],d[2]));
+printf("\nmax = %g", max(d[0],d[1],d[2]));
+printf("\nmed = %g", med(d[0],d[1],d[2]));
+printf("\navg = %g", avg(d[0],d[1],d[2]));
 }
