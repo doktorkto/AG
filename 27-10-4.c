a8e17cd518cc109ce743319cfd490d78115ccbe1
doktorkto <32988757+doktorkto@users.noreply.github.com>
2017-10-29 16:54:43 +0300

    27-10-4

--- 27-10-4.c
+++ 27-10-4.c
@@ -11,13 +11,14 @@ int main() {
         scanf("%d", &a[i]);
     }
     int m;
-    m=a[0]+a[4];
+    m=a[0]+a[5];
     for (int i=0;i<=n-5;i++) {
         for (int j=i+5;j<=n;j++){
             if (a[i]+a[j]<m) {
                     m=a[i]+a[j];
-                    printf("%d %d\n",a[i], a[j]);}
+                    printf("%d+%d=",a[i], a[j]);}
         }
     }
+    if (m==a[0]+a[5]) printf("%d+%d=",a[0], a[5]);
     printf("%d", m);
 }
